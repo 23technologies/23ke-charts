1a8f3869c3b5d79367f651a2fd4bdc48bc03a591
Jens Schneider <schneider@23technologies.cloud>
2022-02-17 16:01:58 +0100

    Rename charts so that their names appear more intuitive
    Signed-off-by: Jens Schneider <schneider@23technologies.cloud>

--- hack/helmchart-import/import-gardener-charts.py
+++ hack/helmchart-import/import-gardener-charts.py
@@ -15,24 +15,30 @@ config = [
         "package": "gardener/gardener",
         "version": "v1.38.2",
         "dirs": [
-            {"src": "charts/gardener/controlplane", "dst": "gardener-controlplane"},
-            {"src": "charts/gardener/gardenlet", "dst": "gardenlet"},
+            {
+                "src": "charts/gardener/controlplane",
+                "chart_name": "gardener-controlplane",
+            },
+            {"src": "charts/gardener/gardenlet", "chart_name": "gardenlet"},
         ],
     },
     {
         "package": "gardener/garden-setup",
         "version": "3.19.0",
         "dirs": [
-            {"src": "components/etcd/cluster/chart", "dst": "garden-etcd"},
-            {"src": "components/kube-apiserver/chart", "dst": "kube-apiserver"},
+            {"src": "components/etcd/cluster/chart", "chart_name": "garden-etcd"},
+            {
+                "src": "components/kube-apiserver/chart",
+                "chart_name": "garden-kube-apiserver",
+            },
         ],
     },
     {
         "package": "gardener/dashboard",
         "version": "1.52.3",
         "dirs": [
-            {"src": "charts/gardener-dashboard", "dst": "gardener-dashboard"},
-            {"src": "charts/identity", "dst": "identity"},
+            {"src": "charts/gardener-dashboard", "chart_name": "gardener-dashboard"},
+            {"src": "charts/identity", "chart_name": "identity"},
         ],
     },
 ]
@@ -45,6 +51,17 @@ def update_chart_version(filename, version):
     with open(filename, "r") as f:
         content = f.read()
         content = re.sub("version:.*", "version: " + version, content, flags=re.M)
+
+    with open(filename, "w") as f:
+        f.write(content)
+
+
+def update_chart_name(filename, name):
+    """Update the chart name so that it matches our naming convention, i.e. we define the names in this import script."""
+    # in case our version contains a character like e.g. v1.0.3
+    with open(filename, "r") as f:
+        content = f.read()
+        content = re.sub("name:.*", "name: " + name, content, flags=re.M)
 
     with open(filename, "w") as f:
         f.write(content)
@@ -63,11 +80,16 @@ def import_charts(config, target_dir):
     # clone the repo to the temp dir and copy the specified directories to the target directory
     git.Repo.clone_from(git_url, repo_dir, multi_options=multi_options)
     for dir in config["dirs"]:
-        shutil.rmtree(target_dir + dir["dst"], ignore_errors=True)
-        shutil.copytree(repo_dir + dir["src"], target_dir + dir["dst"])
-        charts = glob.glob(target_dir + dir["dst"] + "/**/Chart.yaml", recursive=True)
+        shutil.rmtree(target_dir + dir["chart_name"], ignore_errors=True)
+        shutil.copytree(repo_dir + dir["src"], target_dir + dir["chart_name"])
+        charts = glob.glob(
+            target_dir + dir["chart_name"] + "/**/Chart.yaml", recursive=True
+        )
         for chart in charts:
             update_chart_version(chart, config["version"])
+        charts = glob.glob(target_dir + dir["chart_name"] + "/Chart.yaml")
+        for chart in charts:
+            update_chart_name(chart, dir["chart_name"])
 
     # delete the temporary directory
     shutil.rmtree(repo_dir)
