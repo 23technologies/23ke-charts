568e9c88426b9e607ec285cfc1bd1240acfe7391
Malte Münch <muench@23technologies.cloud>
2022-03-02 15:52:23 +0100

    Enables script for extension charts
    Signed-off-by: Malte Münch <muench@23technologies.cloud>

--- .github/renovate-post-upgrade.sh
+++ .github/renovate-post-upgrade.sh
@@ -15,14 +15,25 @@ then
     echo "chmod -R 777 /tmp/renovate/repos/github/23technologies/23ke-charts" >> /tmp/tmpscript
     mv /tmp/tmpscript /tmp/scripts/renovate-post-upgrade-script
 fi
-#if echo "$CHANGED_FILE" | grep "values.yaml"
-#then
-#    # looks like gardener-extensions has been updated, import charts:
-#
-#fi
 until test ! -f /tmp/scripts/renovate-post-upgrade-script
 do
-    echo "Custom scripts still running..."
+    echo "Importing gardener charts..."
+    sleep 1
+done
+if echo "$CHANGED_FILE" | grep "import-gardener-extension-charts.py"
+then
+    # looks like gardener-extensions has been updated, import charts:
+    echo 'cd /tmp/renovate/repos/github/23technologies/23ke-charts' > /tmp/tmpscript
+    echo 'git status -s' >> /tmp/tmpscript
+    echo 'pip install -r hack/helmchart-import/requirements.txt' >> /tmp/tmpscript
+    echo 'python3 hack/helmchart-import/import-gardener-extension-charts.py' >> /tmp/tmpscript
+    echo 'git apply hack/helmchart-import/patches/*.patch' >> /tmp/tmpscript
+    echo "chmod -R 777 /tmp/renovate/repos/github/23technologies/23ke-charts" >> /tmp/tmpscript
+    mv /tmp/tmpscript /tmp/scripts/renovate-post-upgrade-script
+fi
+until test ! -f /tmp/scripts/renovate-post-upgrade-script
+do
+    echo "Importing gardener exetensionexetension  charts..."
     sleep 1
 done
 exit 0
