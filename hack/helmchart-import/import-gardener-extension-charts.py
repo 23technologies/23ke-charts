7cf36c64071479ea48cbc68f3c9dd1088055649d
23t-renovate <renovate@23technologies.cloud>
2022-03-10 10:11:58 +0000

    Update dependency gardener/gardener-extension-provider-aws to v1.34.2

--- hack/helmchart-import/import-gardener-extension-charts.py
+++ hack/helmchart-import/import-gardener-extension-charts.py
@@ -57,7 +57,7 @@ config = [
     },
     {
         "package": "gardener/gardener-extension-provider-aws",
-        "version": "v1.34.1",
+        "version": "v1.34.2",
         "name": "provider-aws",
     },
     {
