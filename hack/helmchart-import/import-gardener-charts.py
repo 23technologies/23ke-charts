f641b974618aeefaf6b2a62ede295b50cd7fe706
Malte Münch <muench@23technologies.cloud>
2022-04-05 14:44:57 +0200

    Manually add gardener v1.42.5
    Signed-off-by: Malte Münch <muench@23technologies.cloud>

--- hack/helmchart-import/import-gardener-charts.py
+++ hack/helmchart-import/import-gardener-charts.py
@@ -14,7 +14,7 @@ target_dir = "charts/"
 config = [
     {
         "package": "gardener/gardener",
-        "version": "v1.44.0",
+        "version": "v1.42.5",
         "dirs": [
             {
                 "src": "charts/gardener/controlplane",
