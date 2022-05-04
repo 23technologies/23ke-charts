661afa4d92229911036f81a98cb04aab1fb280b6
23t-renovate <renovate@23technologies.cloud>
2022-05-04 09:43:15 +0000

    Update dependency gardener/gardener to v1.43.4

--- hack/helmchart-import/import-gardener-charts.py
+++ hack/helmchart-import/import-gardener-charts.py
@@ -14,7 +14,7 @@ target_dir = "charts/"
 config = [
     {
         "package": "gardener/gardener",
-        "version": "v1.43.0",
+        "version": "v1.43.4",
         "dirs": [
             {
                 "src": "charts/gardener/controlplane",
