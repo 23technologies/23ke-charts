6818ac91b3db055a588236a03d1988d6e157562c
23t-renovate <renovate@23technologies.cloud>
2022-05-10 16:02:13 +0000

    Update dependency gardener/gardener-extension-provider-alicloud to v1.35.1

--- hack/helmchart-import/import-gardener-extension-charts.py
+++ hack/helmchart-import/import-gardener-extension-charts.py
@@ -52,7 +52,7 @@ config = [
     },
     {
         "package": "gardener/gardener-extension-provider-alicloud",
-        "version": "v1.35.0",
+        "version": "v1.35.1",
         "name": "provider-alicloud",
     },
     {
