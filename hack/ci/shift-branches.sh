4c3a36b7c4039c2b18f0655440b164d85ce567ee
Malte Münch <muench@23technologies.cloud>
2022-05-16 12:29:59 +0200

    Adds branch-shifting script
    Signed-off-by: Malte Münch <muench@23technologies.cloud>

---
+++ hack/ci/shift-branches.sh
@@ -0,0 +1,37 @@
+#/usr/bin/env bash
+
+## Adjust renovate config:
+# attention: this script assumes that no major release is created.
+git checkout renovate/main-gardener
+LATEST_MINOR=$(python hack/helmchart-import/import-gardener-charts.py show_gardener_version | cut -d '.' -f 2)
+LATEST_1_MINOR=$(( $LATEST_MINOR - 1 ))
+export LATEST_1_REGEX="/^v1\.$LATEST_1_MINOR\..*$/"
+LATEST_2_MINOR=$(( $LATEST_MINOR - 2 ))
+export LATEST_2_REGEX="/^v1\.$LATEST_2_MINOR\..*$/"
+LATEST_3_MINOR=$(( $LATEST_MINOR - 3 ))
+export LATEST_3_REGEX="/^v1\.$LATEST_3_MINOR\..*$/"
+yq eval -o=json -i '.packageRules[3].allowedVersions = env(LATEST_1_REGEX)' .github/renovate.json
+yq eval -o=json -i '.packageRules[4].allowedVersions = env(LATEST_2_REGEX)' .github/renovate.json
+yq eval -o=json -i '.packageRules[5].allowedVersions = env(LATEST_3_REGEX)' .github/renovate.json
+git add .github/renovate.json
+git commit -s -m "Update renovate.json to track Gardener 1.$LATEST_MINOR and its three predecessors"
+git push
+
+## Update branches:
+git checkout latest-3
+git checkout latest-2 hack/helmchart-import/import-gardener-charts.py
+git add hack/helmchart-import/import-gardener-charts.py
+git commit "Shift latest-2 version to latest-3"
+git push
+
+git checkout latest-2
+git checkout latest-1 hack/helmchart-import/import-gardener-charts.py
+git add hack/helmchart-import/import-gardener-charts.py
+git commit "Shift latest-1 version to latest-2"
+git push
+
+git checkout latest-1
+git checkout main hack/helmchart-import/import-gardener-charts.py
+git add hack/helmchart-import/import-gardener-charts.py
+git commit "Shift main version to latest-1"
+git push
