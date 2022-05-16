0e010f63976f21136c6df3b68d990b109b384145
Malte Münch <muench@23technologies.cloud>
2022-05-16 15:50:15 +0200

    fs
    Signed-off-by: Malte Münch <muench@23technologies.cloud>

--- hack/ci/shift-branches.sh
+++ hack/ci/shift-branches.sh
@@ -32,18 +32,21 @@ git push
 
 ## Update branches:
 git checkout latest-3
+git pull
 git checkout latest-2 hack/helmchart-import/import-gardener-charts.py
 git add hack/helmchart-import/import-gardener-charts.py
 git commit "Shift latest-2 version to latest-3"
 git push
 
 git checkout latest-2
+git pull
 git checkout latest-1 hack/helmchart-import/import-gardener-charts.py
 git add hack/helmchart-import/import-gardener-charts.py
 git commit "Shift latest-1 version to latest-2"
 git push
 
 git checkout latest-1
+git pull
 git checkout main hack/helmchart-import/import-gardener-charts.py
 git add hack/helmchart-import/import-gardener-charts.py
 git commit "Shift main version to latest-1"
