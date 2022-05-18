5eb4bacc70a69c87816240f1e21e3bbefd727851
Malte Münch <muench@23technologies.cloud>
2022-05-18 10:41:37 +0200

    Shift latest-1 version to latest-2
    Signed-off-by: Malte Münch <muench@23technologies.cloud>

--- hack/helmchart-import/import-gardener-charts.py
+++ hack/helmchart-import/import-gardener-charts.py
@@ -14,7 +14,7 @@ target_dir = "charts/"
 config = [
     {
         "package": "gardener/gardener",
-        "version": "v1.43.5",
+        "version": "v1.44.6",
         "dirs": [
             {
                 "src": "charts/gardener/controlplane",
