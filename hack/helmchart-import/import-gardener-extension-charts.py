7f1225bc031780ebed6c46784c76739a4d9bbcdc
23t-renovate <renovate@23technologies.cloud>
2022-06-20 16:27:03 +0000

    chore(deps): update dependency gardener/external-dns-management to v0.12.6

--- hack/helmchart-import/import-gardener-extension-charts.py
+++ hack/helmchart-import/import-gardener-extension-charts.py
@@ -32,7 +32,7 @@ config = [
     },
     {
         "package": "gardener/external-dns-management",
-        "version": "v0.12.5",
+        "version": "v0.12.6",
         "name": "external-dns-management",
     },
     {
