0a9c1e74ab0dfc4bf32510a650a8a03cc456a09d
Malte Münch <muench@23technologies.cloud>
2022-07-06 12:56:31 +0200

    Adjust renovate config to use the new helmchart-import-go program
    Signed-off-by: Malte Münch <muench@23technologies.cloud>

--- .github/renovate-post-upgrade.sh
+++ .github/renovate-post-upgrade.sh
@@ -2,9 +2,9 @@
 
 # First, find out which file has been changed:
 
-CHANGED_FILE=$(git status -s)
+CHANGED_FILES=$(git status -s)
 
-if echo "$CHANGED_FILE" | grep "import-gardener-charts.py"
+if echo "$CHANGED_FILES" | grep "import-gardener-charts.py"
 then
     # looks like gardener-core has been updated, import charts:
     echo 'cd /tmp/renovate/repos/github/23technologies/23ke-charts' > /tmp/tmpscript
@@ -20,7 +20,8 @@ do
     echo "Importing gardener charts..."
     sleep 1
 done
-if echo "$CHANGED_FILE" | grep "import-gardener-extension-charts.py"
+
+if echo "$CHANGED_FILES" | grep "import-gardener-extension-charts.py"
 then
     # looks like gardener-extensions has been updated, import charts:
     echo 'cd /tmp/renovate/repos/github/23technologies/23ke-charts' > /tmp/tmpscript
@@ -34,6 +35,24 @@ fi
 until test ! -f /tmp/scripts/renovate-post-upgrade-script
 do
     echo "Importing gardener exetensionexetension  charts..."
+    sleep 1
+done
+
+if echo "$CHANGED_FILES" | grep "config.yaml"
+then
+    # looks like the hemlchart-importer-go config.yaml has been changed
+    echo 'cd /tmp/renovate/repos/github/23technologies/23ke-charts' > /tmp/tmpscript
+    echo 'git status -s' >> /tmp/tmpscript
+    echo 'cd hack/helmchart-import/helmchart-import-go/' >> /tmp/tmpscript
+    echo 'go run . --target ../../../distinct-charts' >> /tmp/tmpscript
+    echo 'cd /tmp/renovate/repos/github/23technologies/23ke-charts' >> /tmp/tmpscript
+    echo 'git apply hack/helmchart-import/patches/*.patch' >> /tmp/tmpscript
+    echo "chmod -R 777 /tmp/renovate/repos/github/23technologies/23ke-charts" >> /tmp/tmpscript
+    mv /tmp/tmpscript /tmp/scripts/renovate-post-upgrade-script
+fi
+until test ! -f /tmp/scripts/renovate-post-upgrade-script
+do
+    echo "Running helmchart-go importer..."
     sleep 1
 done
 exit 0
