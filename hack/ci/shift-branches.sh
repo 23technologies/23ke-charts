e5dba80f65701ec631d28fe11e0f5bee0d1d7b28
Malte Münch <muench@23technologies.cloud>
2022-07-19 11:37:50 +0200

    Fix branch-shifting
    Signed-off-by: Malte Münch <muench@23technologies.cloud>

--- hack/ci/shift-branches.sh
+++ hack/ci/shift-branches.sh
@@ -45,21 +45,24 @@ git pull
 ## Update branches:
 git checkout latest-3
 git pull
-git checkout latest-2 hack/helmchart-import/import-gardener-charts.py
+#git checkout latest-2 hack/helmchart-import/import-gardener-charts.py
+sed "18s/\"version\": \"v[0-9]*\.[0-9]*\.[0-9]*\",/\"version\": \"v1.$LATEST_3_MINOR.0\",/" hack/helmchart-import/import-gardener-charts.py
 git add hack/helmchart-import/import-gardener-charts.py
 git commit -s -m "Shift latest-2 version to latest-3"
 git push
 
 git checkout latest-2
 git pull
-git checkout latest-1 hack/helmchart-import/import-gardener-charts.py
+#git checkout latest-1 hack/helmchart-import/import-gardener-charts.py
+sed "18s/\"version\": \"v[0-9]*\.[0-9]*\.[0-9]*\",/\"version\": \"v1.$LATEST_2_MINOR.0\",/" hack/helmchart-import/import-gardener-charts.py
 git add hack/helmchart-import/import-gardener-charts.py
 git commit -s -m "Shift latest-1 version to latest-2"
 git push
 
 git checkout latest-1
 git pull
-git checkout main hack/helmchart-import/import-gardener-charts.py
+#git checkout main hack/helmchart-import/import-gardener-charts.py
+sed "18s/\"version\": \"v[0-9]*\.[0-9]*\.[0-9]*\",/\"version\": \"v1.$LATEST_1_MINOR.0\",/" hack/helmchart-import/import-gardener-charts.py
 git add hack/helmchart-import/import-gardener-charts.py
 git commit -s -m "Shift main version to latest-1"
 git push
