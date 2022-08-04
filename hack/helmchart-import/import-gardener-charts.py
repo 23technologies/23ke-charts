9810fffa6630428a3a5e47066eeffa0a5663de6e
23t-renovate <renovate@23technologies.cloud>
2022-08-04 11:58:37 +0000

    chore(deps): update dependency gardener/garden-setup to v3.31.0

--- hack/helmchart-import/import-gardener-charts.py
+++ hack/helmchart-import/import-gardener-charts.py
@@ -41,7 +41,7 @@ config = [
     },
     {
         "package": "gardener/garden-setup",
-        "version": "3.30.0",
+        "version": "3.31.0",
         "dirs": [
             {
                 "src": "components/etcd/cluster/chart",
